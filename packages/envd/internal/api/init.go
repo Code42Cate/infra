30c96b079239387bc359a28e7cbd41104e02cb9c
Jonas Scholz <Jonas.Scholz@bbscholz.de>
2025-08-21 23:40:25 +0200

    document init

--- packages/envd/internal/api/init.go
+++ packages/envd/internal/api/init.go
@@ -92,6 +92,8 @@ func (a *API) PostInit(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusNoContent)
 }
 
+// installCertificate installs the provided certificate into the trusted certificate store
+// this is basically like running update-ca-certificates, but much faster
 func installCertificate(certificate string, logger zerolog.Logger) error {
 
 	certData := []byte(certificate)
@@ -128,23 +130,27 @@ func installCertificate(certificate string, logger zerolog.Logger) error {
 		return fmt.Errorf("failed to write to bundle: %w", err)
 	}
 
-	// 3. Create hash symlink for OpenSSL compatibility
+	// 3. Create hash symlink for OpenSSL to make faster lookups
 	block, _ := pem.Decode(certData)
-	if block != nil {
-		cert, err := x509.ParseCertificate(block.Bytes)
-		if err == nil {
-			hash := sha1.Sum(cert.RawSubject)
-			hashValue := binary.LittleEndian.Uint32(hash[:4])
-			hashLink := filepath.Join(certsDir, fmt.Sprintf("%08x.0", hashValue))
-			os.Remove(hashLink) // Remove if exists
-			if err := os.Symlink("e2b.pem", hashLink); err != nil {
-				logger.Debug().Msgf("Failed to create hash symlink: %v", err)
-			}
-		} else {
-			logger.Debug().Msgf("Failed to parse certificate: %v", err)
+	// Not super critical if this should fail for some reason, MITM will still work
+	if block == nil {
+		logger.Debug().Msg("Failed to decode PEM block from certificate")
+		return nil
+	}
+
+	cert, err := x509.ParseCertificate(block.Bytes)
+	if err == nil {
+		// This creates hash like 607eca47.0 -> e2b.pem
+		hash := sha1.Sum(cert.RawSubject)
+		hashValue := binary.LittleEndian.Uint32(hash[:4])
+		hashLink := filepath.Join(certsDir, fmt.Sprintf("%08x.0", hashValue))
+
+		os.Remove(hashLink) // Remove if exists
+		if err := os.Symlink("e2b.pem", hashLink); err != nil {
+			logger.Debug().Msgf("Failed to create hash symlink: %v", err)
 		}
 	} else {
-		logger.Debug().Msg("Failed to decode PEM block from certificate")
+		logger.Debug().Msgf("Failed to parse certificate: %v", err)
 	}
 
 	return nil
